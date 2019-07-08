c96401144396eef0e51c57cbb6ebdf79fdc5b4e1
zxhbloomer <zxhbloomer@gmail.com>
2019-07-08 17:14:44 +0800

    2019070801

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -35,6 +35,7 @@ const actions = {
     return new Promise((resolve, reject) => {
       loginApi({ username: username.trim(), password: password }).then(response => {
         const { data } = response
+        // 设置令牌
         commit('SET_TOKEN', data.token)
         setToken(data.token)
         resolve()
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
+// upd by zxh
+// import { MessageBox, Message } from 'element-ui'
+import { MessageBox } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
 
@@ -44,14 +46,15 @@ service.interceptors.response.use(
    */
   response => {
     const res = response.data
-
     // if the custom code is not 20000, it is judged as an error.
     if (res.code !== 20000) {
-      Message({
-        message: res.message || 'Error',
-        type: 'error',
-        duration: 5 * 1000
-      })
+      // upd by zxh
+      // 调用成功时，不需要message
+      // Message({
+      //   message: res.message || 'Error',
+      //   type: 'error',
+      //   duration: 5 * 1000
+      // })
 
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
@@ -66,7 +69,9 @@ service.interceptors.response.use(
           })
         })
       }
-      return Promise.reject(new Error(res.message || 'Error'))
+      // upd by zxh
+      // return Promise.reject(new Error(res.message || 'Error'))
+      return Promise.resolve(res.data)
     } else {
       return res
     }
