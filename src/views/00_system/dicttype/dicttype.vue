afdd324222e416a4f3a63e8cb8fe6d9c3fdab9cf
zxhbloomer@gmail.com <Lyyzxh030324>
2019-08-30 07:23:46 +0800

    。。

--- src/views/00_system/dicttype/dicttype.vue
+++ src/views/00_system/dicttype/dicttype.vue
@@ -57,8 +57,8 @@
     >
       <el-table-column type="selection" width="38" :reserve-selection="true" prop="id" />
       <el-table-column type="index" width="38" />
-      <el-table-column show-overflow-tooltip sortable="custom" min-width="80" :sort-orders="settings.sortOrders" prop="name" label="名称" />
-      <el-table-column show-overflow-tooltip sortable="custom" min-width="150" :sort-orders="settings.sortOrders" prop="code" label="编码" />
+      <el-table-column show-overflow-tooltip sortable="custom" min-width="80" :sort-orders="settings.sortOrders" prop="name" label="字典名称" />
+      <el-table-column show-overflow-tooltip sortable="custom" min-width="150" :sort-orders="settings.sortOrders" prop="code" label="字典编码" />
       <el-table-column show-overflow-tooltip min-width="150" prop="descr" label="描述" />
       <el-table-column min-width="35" :sort-orders="settings.sortOrders" label="删除">
         <template slot-scope="scope">
@@ -96,68 +96,43 @@
         label-width="120px"
         status-icon
       >
-        <el-steps :active="stepsSetting.active" finish-status="success" align-center>
-          <el-step title="选择资源类型" />
-          <el-step title="输入资源数据" />
-        </el-steps>
-        <br>
-        <br>
-        <div v-show="stepsSetting.active === 0">
-          <el-form-item label="资源类型：" prop="type">
-            <el-select v-model="dataJson.tempJson.type" placeholder="请选择资源类型" clearable @change="handleSelectChange">
-              <el-option
-                v-for="item in settings.codeOptions"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              />
-            </el-select>
-          </el-form-item>
-        </div>
-        <div v-show="stepsSetting.active === 1">
-          <el-row>
-            <el-col :span="12">
-              <el-form-item label="资源类型：" prop="type">
-                <el-input v-model.trim="dataJson.tempJson.type" disabled />
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item label="资源名称：" prop="name">
-                <el-input v-model.trim="dataJson.tempJson.name" clearable show-word-limit :maxlength="dataJson.inputSettings.maxLength.name" />
-              </el-form-item>
-            </el-col>
-          </el-row>
-          <el-form-item label="描述：" prop="descr">
-            <el-input v-model.trim="dataJson.tempJson.descr" clearable type="textarea" show-word-limit :maxlength="dataJson.inputSettings.maxLength.descr" />
-          </el-form-item>
-          <el-form-item label="JSON配置信息：" prop="context">
-            <el-input v-model.trim="dataJson.tempJson.context" :autosize="{ minRows: 4, maxRows: 10 }" clearable type="textarea" show-word-limit />
-          </el-form-item>
-          <el-row>
-            <el-col :span="12">
-              <el-form-item label="更新者：" prop="uId">
-                <el-input v-model.trim="dataJson.tempJson.uId" disabled />
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item label="更新时间：" prop="uTime">
-                <el-input v-model.trim="dataJson.tempJson.uTime" disabled />
-              </el-form-item>
-            </el-col>
-          </el-row>
-        </div>
-
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="字典名称：" prop="name">
+              <el-input v-model.trim="dataJson.tempJson.name" clearable show-word-limit :maxlength="dataJson.inputSettings.maxLength.name" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="字典编码：" prop="code">
+              <el-input v-model.trim="dataJson.tempJson.code" clearable show-word-limit :maxlength="dataJson.inputSettings.maxLength.code" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-form-item label="描述：" prop="descr">
+          <el-input v-model.trim="dataJson.tempJson.descr" clearable type="textarea" show-word-limit :maxlength="dataJson.inputSettings.maxLength.descr" />
+        </el-form-item>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="更新者：" prop="uId">
+              <el-input v-model.trim="dataJson.tempJson.uId" disabled />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="更新时间：" prop="uTime">
+              <el-input v-model.trim="dataJson.tempJson.uTime" disabled />
+            </el-form-item>
+          </el-col>
+        </el-row>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-divider />
         <div class="floatLeft">
-          <el-button v-show="stepsSetting.active === stepsSetting.stepNumber" type="danger" :disabled="settings.listLoading" @click="doReset()">重置</el-button>
+          <el-button type="danger" :disabled="settings.listLoading" @click="doReset()">重置</el-button>
         </div>
         <el-button plain :disabled="settings.listLoading" @click="popSettingsData.dialogFormVisible = false">取 消</el-button>
-        <el-button v-show="stepsSetting.active !== stepsSetting.stepNumber" @click="handleNext">下一步</el-button>
-        <el-button v-show="popSettingsData.btnStatus.doInsert && stepsSetting.active === stepsSetting.stepNumber" plain type="primary" :disabled="settings.listLoading" @click="doInsert()">确 定</el-button>
-        <el-button v-show="popSettingsData.btnStatus.doUpdate && stepsSetting.active === stepsSetting.stepNumber" plain type="primary" :disabled="settings.listLoading" @click="doUpdate()">确 定</el-button>
-        <el-button v-show="popSettingsData.btnStatus.doCopyInsert && stepsSetting.active === stepsSetting.stepNumber" plain type="primary" :disabled="settings.listLoading" @click="doCopyInsert()">确 定</el-button>
+        <el-button v-show="popSettingsData.btnStatus.doInsert" plain type="primary" :disabled="settings.listLoading" @click="doInsert()">确 定</el-button>
+        <el-button v-show="popSettingsData.btnStatus.doUpdate" plain type="primary" :disabled="settings.listLoading" @click="doUpdate()">确 定</el-button>
+        <el-button v-show="popSettingsData.btnStatus.doCopyInsert" plain type="primary" :disabled="settings.listLoading" @click="doCopyInsert()">确 定</el-button>
       </div>
     </el-dialog>
   </div>
@@ -223,8 +198,8 @@ export default {
         tempJson: null,
         inputSettings: {
           maxLength: {
-            name: 10,
-            code: 50,
+            name: 20,
+            code: 20,
             descr: 200,
             dbversion: 0
           }
@@ -266,53 +241,21 @@ export default {
         dialogStatus: '',
         dialogFormVisible: false,
         // pop的check内容
-        rules: []
-      },
-      // 步骤设置部分
-      stepsSetting: {
-        active: 0, // 控制步骤
-        stepNumber: 1, // 总步数
-        // 步骤1的check内容
-        rulesFirst: {
-          type: [
-            { required: true, message: '请选择资源类型', trigger: 'change' },
-            { validator: this.validateType, trigger: 'change' }
-          ]
-        },
-        // 步骤2的check内容
-        rulesSecond: {
-          name: [{ required: true, message: '请输入资源名称', trigger: 'change' }],
-          context: [{ required: true, message: '请输入json配置信息', trigger: 'change' }]
+        rules: {
+          name: [{ required: true, message: '请输入字典名称', trigger: 'change' }],
+          code: [{ required: true, message: '请输入字典编码', trigger: 'change' }]
         }
       }
     }
   },
   // 监听器
   watch: {
-    // 根据当前步骤，替换相应validate的rules
-    'stepsSetting.active': {
-      handler(newVal, oldVal) {
-        this.$nextTick(() => {
-          this.$refs['dataSubmitForm'].clearValidate()
-        })
-        switch (newVal) {
-          case 0:
-            this.popSettingsData.rules = this.stepsSetting.rulesFirst
-            break
-          case 1:
-            this.popSettingsData.rules = this.stepsSetting.rulesSecond
-            break
-        }
-      }
-    }
   },
   created() {
     // 初始化查询
     this.getDataList()
     // 数据初始化
     this.dataJson.tempJson = Object.assign({}, this.dataJson.tempJsonOriginal)
-    // 步骤初始化
-    this.popSettingsData.rules = this.stepsSetting.rulesFirst
   },
   methods: {
     // 下拉选项控件事件
@@ -421,8 +364,6 @@ export default {
       this.popSettingsData.btnStatus.doInsert = false
       this.popSettingsData.btnStatus.doUpdate = true
       this.popSettingsData.btnStatus.doCopyInsert = false
-      // 设置步骤激活的步骤：2
-      this.stepsSetting.active = 1
     },
     // 导出按钮
     handleExport() {
@@ -558,15 +499,6 @@ export default {
     },
     // 重置按钮
     doReset() {
-      // 步骤初始化
-      switch (this.popSettingsData.dialogStatus) {
-        case 'insert':
-          this.stepsSetting.active = 0
-          break
-        case 'update':
-          this.stepsSetting.active = 1
-          break
-      }
       // 数据初始化
       this.dataJson.tempJson.name = ''
       this.dataJson.tempJson.descr = ''
@@ -616,23 +548,6 @@ export default {
     // table选择框
     handleSelectionChange(val) {
       this.dataJson.multipleSelection = val
-    },
-    handleNext() {
-      this.$refs['dataSubmitForm'].validate((valid) => {
-        if (valid) {
-          // check没有错误
-          if (this.stepsSetting.active === this.stepsSetting.stepNumber) {
-            return
-          }
-          this.stepsSetting.active++
-          this.$nextTick(() => {
-            this.$refs['dataSubmitForm'].clearValidate()
-          })
-        } else {
-          // check有错误
-          return false
-        }
-      })
     },
     // 资源类型check
     validateType(rule, value, callback) {
